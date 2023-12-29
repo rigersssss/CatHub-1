33666121fd9c4cffdf4a0a121b54794990ad6f18
BZajc <bbb.zaj@gmail.com>
2023-12-29 21:29:52 +0100

    Optimized code for Pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,52 +1,84 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCatImagesAsync, selectUserSelectedBreed } from "../store/slices/catImageSlice";
+import { useState, useEffect, useCallback } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Pagination() {
-  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageButtons, setPageButtons] = useState(getPageButtons());
+
   const navigate = useNavigate();
-  const dispatch = useDispatch()
-  const selectedBreed = useSelector(selectUserSelectedBreed)
+  const location = useLocation();
 
-  const handleButtonClick = (index) => {
-    if (index === 0) {
-      setNumbers([1, 2, 3, 4, 5]);
-      navigate(`/`);
-    } else if (index === 1 && numbers[1] !== 2) {
-      setNumbers(numbers.map((num, i) => (i === 0 ? num : num - 1)));
-    } else if (index >= 2) {
-      const incrementValue = index - 2;
-      setNumbers(numbers.map((num, i) => (i >= 1 ? num + incrementValue : num)));
-    }
-
-    navigate(`/page/${numbers[index]}`);
-    dispatch(fetchCatImagesAsync(selectedBreed.id))
-  };
-
-  const handlePrevNextClick = (operation) => {
-    if (operation === "Prev" && numbers[1] === 2) {
-      return;
-    }
-
-    const incrementValue = operation === "Next" ? 1 : -1;
-    setNumbers(numbers.map((num, i) => (i >= 1 ? num + incrementValue : num)));
-  };
+  const handleWindowResize = useCallback(() => {
+    setPageButtons(getPageButtons());
+  }, []);
 
   useEffect(() => {
-    const currentPage = getPageFromUrl();
-    if (currentPage !== null) {
-      if (currentPage === 0) {
-        navigate(`/`);
-      } else {
-        navigate(`/page/${currentPage}`);
-      }
-    }
-  }, [numbers, navigate]);
+    // Getting number of page from URL
+    const getPageNumberFromPath = () => {
+      const match = location.pathname.match(/\/page\/(\d+)/);
+      return match ? parseInt(match[1]) : 1;
+    };
 
-  const getPageFromUrl = () => {
-    const match = window.location.pathname.match(/\/page\/(\d+)/);
-    return match ? parseInt(match[1]) : null;
+    // Setting current page as the one from the URL
+    setCurrentPage(getPageNumberFromPath());
+
+    // Update the number of page buttons when the component mounts
+    setPageButtons(getPageButtons());
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleWindowResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [location.pathname, handleWindowResize]);
+
+  // Determine the number of page buttons based on the current width
+  function getPageButtons() {
+    return window.innerWidth > 900 ? 10 : 5;
+  }
+
+  // Creating number buttons
+  const createPagination = () => {
+    const startPage = Math.max(1, currentPage - Math.floor(pageButtons / 2));
+
+    return Array.from({ length: pageButtons }, (_, i) => {
+      const pageNumber = startPage + i;
+      return (
+        <li className="pagination__li" key={i}>
+          <button
+            className={`pagination__option pagination__option--number ${
+              pageNumber === currentPage ? "pagination-active" : ""
+            }`}
+            onClick={() => handlePaginationClick(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        </li>
+      );
+    });
+  };
+
+  const handlePaginationClick = (pageNumber) => {
+    // Scroll smoothly to the top of the page
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  
+    // After 500ms, update the URL
+    setTimeout(() => {
+      setCurrentPage(pageNumber);
+      navigate(`/page/${pageNumber}`);
+    }, 600);
+  };
+
+  const handlePrevClick = () => {
+    setCurrentPage((prevPage) => Math.max(1, prevPage - 1));
+    navigate(`/page/${currentPage - 1}`);
+  };
+
+  const handleNextClick = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    navigate(`/page/${currentPage + 1}`);
   };
 
   return (
@@ -54,31 +86,18 @@ function Pagination() {
       <ul className="pagination__ul">
         <li className="pagination__li">
           <button
-            onClick={() => handlePrevNextClick("Prev")}
             className="pagination__option pagination__option--prev"
+            onClick={handlePrevClick}
+            disabled={currentPage === 1}
           >
             Prev
           </button>
         </li>
-        {numbers.map((number, index) => (
-          <li
-            key={index}
-            className={`pagination__li ${
-              getPageFromUrl() === number ? "pagination-active" : ""
-            }`}
-          >
-            <button
-              onClick={() => handleButtonClick(index)}
-              className={`pagination__option pagination__option--number`}
-            >
-              {number}
-            </button>
-          </li>
-        ))}
+        {createPagination()}
         <li className="pagination__li">
           <button
-            onClick={() => handlePrevNextClick("Next")}
             className="pagination__option pagination__option--next"
+            onClick={handleNextClick}
           >
             Next
           </button>

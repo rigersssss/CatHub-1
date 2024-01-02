43d0f424b6abb17e06fd9b6a89bc7549ef1baa41
BZajc <bbb.zaj@gmail.com>
2024-01-02 11:58:11 +0100

    Finished breedsInfoPage

--- src/components/BreedInfoContainer.js
+++ src/components/BreedInfoContainer.js
@@ -1,22 +1,160 @@
-// Get all tag images
-// const imagesContext = require.context("../images/cats/", false, /\.(jpg)$/);
+import divider from "../images/divider.svg";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+function BreedInfoContainer({ image, catInfo, isActive, handleClick }) {
+  const {
+    id,
+    name,
+    description,
+    temperament,
+    lifespan,
+    weight,
+    energy,
+    health,
+    intelligence,
+    shedding,
+    social,
+    stranger,
+    more,
+  } = catInfo;
 
-import AbyssinianImage from "../images/cats/breed-abyssinian.jpg";
+  const handleBtnClick = () => {
+    if (handleClick) {
+      handleClick();
+    }
+    if (catInfo) {
+      console.log(catInfo.id);
 
-function BreedInfoContainer() {
+      const element = document.getElementById(catInfo.id);
+
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: "smooth" });
+        }, 600);
+      }
+    }
+  };
+
+  const removeQuotes = (input) => {
+    if (typeof input === "string" || input instanceof String) {
+      return input.replace(/"/g, "");
+    }
+    return input;
+  };
+
+  const renderTraitIcons = (traitValue) => {
+    const fullHearts = traitValue;
+    const emptyHearts = 5 - traitValue;
+
+    const heartIcons = [];
+
+    for (let i = 0; i < fullHearts; i++) {
+      heartIcons.push(<FaHeart key={i} className="breeds-info__heart-icon" />);
+    }
+
+    for (let i = 0; i < emptyHearts; i++) {
+      heartIcons.push(<FaRegHeart key={i + fullHearts} className="breeds-info__heart-icon" />);
+    }
+
+    return heartIcons;
+  };
+
 
   return (
-    <div className="breeds-info__cat-container">
-      <button
-        className="breeds-info__cat-button"
-        style={{
-          backgroundImage: `url(${AbyssinianImage})`,
-        }}
-      >
-        <div className="breeds-info__cat-button-background">
-            <p className="breeds-info__cat-button-name">Abyssinian</p>
+    <div
+      className={`breeds-info__cat-container-wrapper ${
+        isActive ? "info-active" : ""
+      }`}
+    >
+      <div id={id} className={`breeds-info__cat-container`}>
+        <button
+          className="breeds-info__cat-button"
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+          onClick={handleBtnClick}
+        >
+          <div className="breeds-info__cat-button-background">
+            <p className="breeds-info__cat-button-name">{name}</p>
+          </div>
+        </button>
+      </div>
+      <div className={`breeds-info__cat-info-box`}>
+        <p className="breeds-info__cat-text breeds-info__cat-text--name">
+          {name}
+        </p>
+        <p className="breeds-info__cat-text breeds-info__cat-text--temperament">
+          {temperament}
+        </p>
+        <p className="breeds-info__cat-section-text">About me</p>
+        <p className="breeds-info__cat-text breeds-info__cat-text--description">
+          {description}
+        </p>
+        <p className="breeds-info__cat-section-text">Basic informations</p>
+        <div className="breeds-info__cat-basic-info">
+          <p className="breeds-info__cat-text breeds-info__cat-text--lifespan">
+            lifespan: {removeQuotes(JSON.stringify(lifespan))} years
+          </p>
+          <p className="breeds-info__cat-text breeds-info__cat-text--weight">
+            weight: {removeQuotes(JSON.stringify(weight.metric))} kg
+          </p>
         </div>
-      </button>
+        <p className="breeds-info__cat-section-text">Traits</p>
+        <div className="breeds-info__cat-traits-info-box">
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Health
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(health)}
+            </p>
+          </div>
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Energy
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(energy)}
+            </p>
+          </div>
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Intelligence
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(intelligence)}
+            </p>
+          </div>
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Social
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(social)}
+            </p>
+          </div>
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Friendly
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(stranger)}
+            </p>
+          </div>
+          <div className="breeds-info__cat-trait">
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-name">
+              Shedding
+            </p>
+            <p className="breeds-info__cat-text breeds-info__cat-text--trait-amount">
+              {renderTraitIcons(shedding)}
+            </p>
+          </div>
+        </div>
+        <p className="breeds-info__cat-text breeds-info__cat-text--more">If you would like to learn more about your favorite breed, please visit the link below</p>
+        <a href={more} className="breeds-info__cat-text breeds-info__cat-text--link">Read more</a>
+        <div className="breeds-info__divider-box">
+          <img src={divider} alt="decorative ornament" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/BreedsInfoPage.js
+++ src/pages/BreedsInfoPage.js
@@ -7,11 +7,105 @@ import {
 import { FiActivity } from "react-icons/fi";
 import { AiOutlineSearch } from "react-icons/ai";
 import BreedInfoContainer from "../components/BreedInfoContainer";
-// Get all tag images
-// const imagesContext = require.context("../images/cats/", false, /\.(jpg)$/);
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  selectCatBreeds,
+  fetchCatBreedsAsync,
+} from "../store/slices/catImageSlice";
 
+// Get all breed images
+const imagesContext = require.context("../images/cats/", false, /\.(jpg)$/);
 
 function BreedsInfoPage() {
+  const catInfo = useSelector(selectCatBreeds);
+  const dispatch = useDispatch();
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  useEffect(() => {
+    if (catInfo.length === 0) {
+      dispatch(fetchCatBreedsAsync());
+    }
+  }, [dispatch, catInfo]);
+
+  // Ignore s p a c e s, CAPITAL LETTERS and-dashes
+  const modifyBreedName = (name) =>
+    name.toLowerCase().replace(/\s/g, "").replace(/-/g, "");
+
+  const handleContainerClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  const breedsArray = [
+    { name: "Abyssinian", id: "abys" },
+    { name: "Aegean", id: "aege" },
+    { name: "American Bobtail", id: "abob" },
+    { name: "American Curl", id: "acur" },
+    { name: "American Shorthair", id: "asho" },
+    { name: "American Wirehair", id: "awir" },
+    { name: "Arabian Mau", id: "amau" },
+    { name: "Australian Mist", id: "amis" },
+    { name: "Balinese", id: "bali" },
+    { name: "Bambino", id: "bamb" },
+    { name: "Bengal", id: "beng" },
+    { name: "Birman", id: "birm" },
+    { name: "Bombay", id: "bomb" },
+    { name: "British Longhair", id: "bslo" },
+    { name: "British Shorthair", id: "bsho" },
+    { name: "Burmese", id: "bure" },
+    { name: "Burmilla", id: "buri" },
+    { name: "California Spangled", id: "cspa" },
+    { name: "Chantilly-Tiffany", id: "ctif" },
+    { name: "Chartreux", id: "char" },
+    { name: "Chausie", id: "chau" },
+    { name: "Cheetoh", id: "chee" },
+    { name: "Colorpoint Shorthair", id: "csho" },
+    { name: "Cornish Rex", id: "crex" },
+    { name: "Cymric", id: "cymr" },
+    { name: "Cyprus", id: "cypr" },
+    { name: "Devon Rex", id: "drex" },
+    { name: "Donskoy", id: "dons" },
+    { name: "Dragon Li", id: "lihu" },
+    { name: "Egyptian Mau", id: "emau" },
+    { name: "European Burmese", id: "ebur" },
+    { name: "Exotic Shorthair", id: "esho" },
+    { name: "Havana Brown", id: "hbro" },
+    { name: "Himalayan", id: "hima" },
+    { name: "Japanese Bobtail", id: "jbob" },
+    { name: "Javanese", id: "java" },
+    { name: "Khao Manee", id: "khao" },
+    { name: "Korat", id: "kora" },
+    { name: "Kurilian", id: "kuri" },
+    { name: "LaPerm", id: "lape" },
+    { name: "Maine Coon", id: "mcoo" },
+    { name: "Malayan", id: "mala" },
+    { name: "Manx", id: "manx" },
+    { name: "Munchkin", id: "munc" },
+    { name: "Nebelung", id: "nebe" },
+    { name: "Norwegian Forest Cat", id: "norw" },
+    { name: "Ocicat", id: "ocic" },
+    { name: "Oriental", id: "orie" },
+    { name: "Persian", id: "pers" },
+    { name: "Pixie-bob", id: "pixi" },
+    { name: "Ragamuffin", id: "raga" },
+    { name: "Ragdoll", id: "ragd" },
+    { name: "Russian Blue", id: "rblu" },
+    { name: "Savannah", id: "sava" },
+    { name: "Scottish Fold", id: "sfol" },
+    { name: "Selkirk Rex", id: "srex" },
+    { name: "Siamese", id: "siam" },
+    { name: "Siberian", id: "sibe" },
+    { name: "Singapura", id: "sing" },
+    { name: "Snowshoe", id: "snow" },
+    { name: "Somali", id: "soma" },
+    { name: "Sphynx", id: "sphy" },
+    { name: "Tonkinese", id: "tonk" },
+    { name: "Toyger", id: "toyg" },
+    { name: "Turkish Angora", id: "tang" },
+    { name: "Turkish Van", id: "tvan" },
+    { name: "York Chocolate", id: "ycho" },
+  ];
+
   return (
     <div className="breeds-info">
       <header className="breeds-info__header">
@@ -23,8 +117,8 @@ function BreedsInfoPage() {
             </h1>
             <div className="breeds-info__header-split-containers">
               <div className="breeds-info__header-split-container breeds-info__header-split-container--1">
-                <FaRegQuestionCircle className="breeds-info__header-text breeds-info__header-text--orange breeds-info__header-text--icon" />
-                <p className="breeds-info__header-text breeds-info__header-text--orange breeds-info__header-text--how">
+                <FaRegQuestionCircle className="breeds-info__header-text breeds-info__header-text--icon" />
+                <p className="breeds-info__header-text breeds-info__header-text--how">
                   How it works?
                 </p>
               </div>
@@ -64,10 +158,29 @@ function BreedsInfoPage() {
         </p>
         <p className="breeds-info__below-header-info breeds-info__below-header-info--orange">
           *Keep in mind that if you want to search for a specific breed, you can
-          always use the <AiOutlineSearch className="breeds-info__glass-icon"/> icon in the top right corner
+          always use the <AiOutlineSearch className="breeds-info__glass-icon" />{" "}
+          icon in the top right corner
         </p>
       </div>
-      <BreedInfoContainer />
+      <div className="breeds-info__cat-info-grid">
+      {catInfo.length > 0 &&
+          breedsArray.map((breed, index) => {
+            // Find matching object in catInfo array
+            const matchingBreed = catInfo.find((cat) => cat.name === breed.name);
+
+            return (
+              <BreedInfoContainer
+                key={breed.id}
+                image={imagesContext(
+                  `./breed-${modifyBreedName(breed.name)}.jpg`
+                )}
+                catInfo={matchingBreed}
+                isActive={index === activeIndex}
+                handleClick={() => handleContainerClick(index)}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 }
